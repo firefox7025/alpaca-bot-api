dd61ac135e541f275b3fad69e3b2595c6752ebc0
Alexander Montgomery <evaderxander@gmail.com>
2019-09-12 22:56:06 -0400

    Added webserver, routes do nothing but have routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate iron;
+extern crate router;
+
+
+use iron::prelude::*;
+use iron::status;
+use iron::Iron;
+use iron::Request;
+use iron::Response;
+use iron::IronResult;
+use router::{Router};
+
 fn main() {
-    println!("Hello, world!");
+    let mut router = Router::new();
+    router.get("/", handler, "handler");
+    router.get("/:query", query_handler, "query_handler");
+
+    Iron::new(router).http("localhost:3000");
+
+    fn handler(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "OK")))
+    }
+
+    fn query_handler(req: &mut Request) -> IronResult<Response> {
+        let ref query = req.extensions.get::<Router>()
+            .unwrap().find("query").unwrap_or("/");
+        Ok(Response::with((status::Ok, *query)))
+    }
 }
